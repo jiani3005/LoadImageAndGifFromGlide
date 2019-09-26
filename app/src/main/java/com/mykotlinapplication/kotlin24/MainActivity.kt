008b08ba188b25331925d841daef4fbc907a5085
Jia Ni Chen <jiani3005@gmail.com>
2019-09-26 15:58:05 -0500

    second commit

--- app/src/main/java/com/mykotlinapplication/kotlin24/MainActivity.kt
+++ app/src/main/java/com/mykotlinapplication/kotlin24/MainActivity.kt
@@ -12,7 +12,18 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val linearLayout = findViewById<LinearLayout>(R.id.linearLayout)
         val imageView = ImageView(this)
-        Glide.with(this).load("https://s3.amazonaws.com/appsdeveloperblog/Micky.jpg").into(imageView)
+
+        // Use this for loading image
+//        Glide.with(this).load("https://s3.amazonaws.com/appsdeveloperblog/Micky.jpg").into(imageView)
+//        linearLayout.addView(imageView)
+
+        // Use this for loading gif
         linearLayout.addView(imageView)
+        Glide.with(this)
+            .asGif()
+            .load("https://s3.amazonaws.com/appsdeveloperblog/micky.gif")
+            .into(imageView)
+
+
     }
 }
