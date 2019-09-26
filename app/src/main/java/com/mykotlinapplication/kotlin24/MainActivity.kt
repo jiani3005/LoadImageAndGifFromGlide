ae9ab9b56b170cb133c97d57fa0bba0b9edd3266
Jia Ni Chen <jiani3005@gmail.com>
2019-09-26 15:48:37 -0500

    first commit

---
+++ app/src/main/java/com/mykotlinapplication/kotlin24/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.mykotlinapplication.kotlin24
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.LinearLayout
+import com.bumptech.glide.Glide
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val linearLayout = findViewById<LinearLayout>(R.id.linearLayout)
+        val imageView = ImageView(this)
+        Glide.with(this).load("https://s3.amazonaws.com/appsdeveloperblog/Micky.jpg").into(imageView)
+        linearLayout.addView(imageView)
+    }
+}
